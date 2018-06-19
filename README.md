7b2e9cf891394b092d3e026c806df83d241f54fe
Emily Morris <morris.emilyanne@gmail.com>
2018-06-18 21:15:33 -0500

    improved a11y and responsive features

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
-#API Hack Capstone
+<b>Close To Home</b>
 
-This is a capstone project for Thinkful that involves building an app that integrates with a third party API. The app will help users find preschools or daycares in their area by melding data from the Yelp API with Google Maps.
+An application for users to find the 10 best rated daycares and preschools in their area.
+
+<b>Motivation</b>
+
+As a new mom, I'm currently looking for the right daycare for my son. I wanted to make an app that would help other parents with this important decision.
+
+<b>Screenshots</b>
+
+Landing page and search form:
+
+<img src="https://cdn.glitch.com/a5bbbe7e-6504-4f3f-830a-77bdc68904c0%2Flanding-page.PNG?1529269431077" alt="landing page">
+
+Map with list of results:
+
+<img src="https://cdn.glitch.com/a5bbbe7e-6504-4f3f-830a-77bdc68904c0%2Fresults-page.PNG?1529270759531" alt="results page">
+
+Map with route and directions:
+
+<img src="https://cdn.glitch.com/a5bbbe7e-6504-4f3f-830a-77bdc68904c0%2Fmap-and-directions.PNG?1529270909009" alt="map and directions">
+
+<b>Built With</b>
+
+Front-end:
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>JavaScript</li>
+  <li>jQuery</li>
+</ul>
+
+Back-end:
+<ul>
+  <li>Node</li>
+  <li>Express</li>
+</ul>
+
+<b>Features</b>
+<ul>
+  <li>Search for child care site by entering address or zip code in search bar</li>
+  <li>See results on map and on sidebar with address and rating</li>
+  <li>Get routes on map by clicking on markers</li>
+  <li>Get directions by showing sidebar</li>
+</ul>
+
+<b>Demo</b>
+<ul>
+  <li>
+  <a href="https://close-to-home.glitch.me">Live Demo</a>
+  </li>
+</ul>
+
+<b>Author</b>
+
+Emily Morris
--- public/index.js
+++ public/index.js
@@ -10,15 +10,6 @@ function loadPage() {
 
 loadPage();
 
-function getSearch() {
-    $('button').click(event => {
-        $('.landing-page').hide();
-        $('.results-page').show();
-    });
-}
-
-getSearch();
-
 function doSearch() {
     $('.js-search-form').submit(event => {
     	event.preventDefault();
@@ -28,174 +19,171 @@ function doSearch() {
         $.get(localPath + '/api-capstone?address=' + $('.js-query').val(), function(data) {
             initMap(data);
         });
-        $('.results-page').show();
-        $('#map').show();
-        console.log($(document).width());
-        if($(document).width() > 767) {
-            $('.sidebar').show();
-            $('.show-bar').hide();
-            $('.close-bar').hide();
-        }
-    });
+       $('.landing-page').hide();
+    $('.results-page').show();
+    $('#map').show();
+    if($(document).width() > 767) {
+      $('.sidebar').show();
+      $('.show-bar').hide();
+      $('.close-bar').hide();
+    }
+  });
 }
 
 doSearch();
 
 function newSearch() {
-    $('.new-search-btn').click(event => {
-        $('.landing-page').show();
-        $('.results-page').hide();
-        $('.sidebar').hide();
-        $('#map').hide();
-        location.reload();
-    });
+  $('.new-search-btn').click(event => {
+      $('.landing-page').show();
+      $('.results-page').hide();
+      $('.sidebar').hide();
+      $('#map').hide();
+      location.reload();
+  });
 }
 
 newSearch();
 
 function showSidebar() {
-    $('.show-bar').click(event => {
-        console.log('click');
-        // $('.btn').hide();
-        $('.sidebar').show();
-        $('.show-bar').show();
-        $('.close-bar').show();
-    });
+  $('.show-bar').click(event => {
+      $('.sidebar').show();
+      $('.close-bar').show();
+  });
 }
 
 showSidebar();
 
 function hideSidebar() {
-    if($(document).width() < 767) {
-        $('.close-bar').click(event => {
-            console.log('click');
-            $('.sidebar').hide();
-        });
-    }
+  if($(document).width() < 767) {
+    $('.close-bar').click(event => {
+        $('.sidebar').hide();
+    });
+  } 
 } 
 
 hideSidebar();
 
- function createGeocoder() {
-    return new google.maps.Geocoder();
+//convert user input to latitude/longitude
+function createGeocoder() {
+  return new google.maps.Geocoder();
 }
 
 function codeAddress(geocoder, map, yelpResults) {
-    let address = $('#address').val();
-    geocoder.geocode(
-        {
-            'address': address
-        },
-        function(results, status) {
-            if(status === 'OK') {
-                homeLoc = results[0].geometry.location;
-                console.log(results[0].geometry.location);
-                map.setCenter(results[0].geometry.location);
-                let marker = new google.maps.Marker({
-                    map: map,
-                    position: results[0].geometry.location
-                });
-            } else {
-                alert('Please enter a valid address');
-            }
-            compileResults(yelpResults, map, results[0].geometry.location);
-        }
-    );
+  let address = $('#address').val();
+  geocoder.geocode(
+      {
+          'address': address
+      },
+      function(results, status) {
+          if(status === 'OK') {
+              homeLoc = results[0].geometry.location;
+              map.setCenter(results[0].geometry.location);
+              let marker = new google.maps.Marker({
+                  map: map,
+                  position: results[0].geometry.location
+              });
+          } else {
+              alert('Please enter a valid address');
+          }
+          compileResults(yelpResults, map, results[0].geometry.location);
+      }
+  );
+}
+
+//set up map with coordinates of search results and user input
+function createMap(lat, lng) {
+  return new google.maps.Map(
+      document.getElementById('map'), {
+          zoom: 11,
+          center: {
+              lat: 32.968288,
+              lng: -96.867130 
+          }
+      }
+  )   
 }
 
 function initMap(yelpResults) {
-    let userInput = $('.js-query').val();
-    let geocoder = createGeocoder();
-    const map = createMap();
-    codeAddress(geocoder, map, yelpResults);   
+  let userInput = $('.js-query').val();
+  let geocoder = createGeocoder();
+  const map = createMap();
+  codeAddress(geocoder, map, yelpResults);   
 }
 
-function compileResults(yelpResults, map, homeLoc) {
-    $('.results').empty();
-    yelpResults.forEach(function(item) {
-        let businessName = item.name;
-        let businessLoc1 = item.location.display_address[0];
-        let businessLoc2 = item.location.display_address[1];
-        let businessRating = item.rating;
-        let resultString = `<li><strong>${businessName}</strong><br/> <em>Address:</em> ${businessLoc1} ${businessLoc2}<br/> <em>Rating:</em> ${businessRating}</li>`;
-        
-        $('.results').append(resultString);
-        
-        updateMarker(item.coordinates.latitude, item.coordinates.longitude, map);
-        
-        let markers = [];
-     
-        function getInfo() {
-            let numMarkers = yelpResults.length;
-            console.log(yelpResults);
-            for(let i = 0; i < numMarkers; i++) {
-                console.log(yelpResults[i].location);
-                markers[i] = new google.maps.Marker({
-                    position: {lat:yelpResults[i].coordinates.latitude, lng:yelpResults[i].coordinates.longitude},
-                    map: map,
-                    html: yelpResults[i].name + '<br/>' + yelpResults[i].location.address1,
-                    id: i
-                });
-                
-                google.maps.event.addListener(markers[i], 'click', function() {
-                    console.log(this);
-                    let infoWindow = new google.maps.InfoWindow({
-                        id: this.id,
-                        content: this.html + '<br/>' + '<input type="button" onClick="getDir(' + yelpResults[i].coordinates.latitude + ',' + yelpResults[i].coordinates.longitude + ')" value="Click for route, sidebar for directions">',
-                        position: this.getPosition()
-                    });
-                    google.maps.event.addListenerOnce(infoWindow, 'closeclick', function() {
-                        markers[this.id].setVisible(true);
-                    });
-                    this.setVisible(false);
-                    infoWindow.open(map);
-                });
-                geocoder = new google.maps.Geocoder();
-                directionsService = new google.maps.DirectionsService();
-                directionsDisplay = new google.maps.DirectionsRenderer({
-                    suppressMarkers: false
-                });
-                directionsDisplay.setMap(map);
-                directionsDisplay.setPanel(document.getElementById('show-results'));
-            }
-        }
-        getInfo();
-        $('.js-query').val('');
-    });
-}
-
-function getDir(resultLat, resultLng) {
-    let destination = new google.maps.LatLng(resultLat, resultLng);
-    let request = {
-        origin: homeLoc,
-        destination: destination,
-        travelMode: google.maps.DirectionsTravelMode.DRIVING
-    };
-    directionsService.route(request, function (response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-            $('.show-results').empty();
-            directionsDisplay.setDirections(response);
-        } 
-    });
-}
-
+//set markers on map
 function updateMarker(lat, lng, map) {
-   marker = new google.maps.Marker(
-        {
-            position: {lat, lng},
-            map
-        }
-    );
+  marker = new google.maps.Marker(
+      {
+          position: {lat, lng},
+          map
+      }
+  );
 }
 
-function createMap(lat, lng) {
-    return new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 11,
-            center: {
-                lat: 32.968288,
-                lng: -96.867130 
-            }
-        }
-    )   
+//get directions from user address to child care site
+function getDir(resultLat, resultLng) {
+  let destination = new google.maps.LatLng(resultLat, resultLng);
+  let request = {
+      origin: homeLoc,
+      destination: destination,
+      travelMode: google.maps.DirectionsTravelMode.DRIVING
+  };
+  directionsService.route(request, function (response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+          $('.show-results').empty();
+          directionsDisplay.setDirections(response);
+      } 
+  });
+}
+
+//set up list of results
+function compileResults(yelpResults, map, homeLoc) {
+  $('.results').empty();
+  yelpResults.forEach(function(item) {
+      let businessName = item.name;
+      let businessLoc1 = item.location.display_address[0];
+      let businessLoc2 = item.location.display_address[1];
+      let businessRating = item.rating;
+      let resultString = `<li><strong>${businessName}</strong><br/> <em>Address:</em> ${businessLoc1} ${businessLoc2}<br/> <em>Rating:</em> ${businessRating}</li>`;
+
+      $('.results').append(resultString);
+
+      updateMarker(item.coordinates.latitude, item.coordinates.longitude, map);
+
+      let markers = [];
+      //create info windows when clicking on markers
+      function getInfo() {
+          let numMarkers = yelpResults.length;
+          for(let i = 0; i < numMarkers; i++) {
+              markers[i] = new google.maps.Marker({
+                  position: {lat:yelpResults[i].coordinates.latitude, lng:yelpResults[i].coordinates.longitude},
+                  map: map,
+                  html: yelpResults[i].name + '<br/>' + yelpResults[i].location.address1,
+                  id: i
+              });
+
+              google.maps.event.addListener(markers[i], 'click', function() {
+                  let infoWindow = new google.maps.InfoWindow({
+                      id: this.id,
+                      content: this.html + '<br/>' + '<input type="button" onClick="getDir(' + yelpResults[i].coordinates.latitude + ',' + yelpResults[i].coordinates.longitude + ')" value="Click for route, sidebar for directions">',
+                      position: this.getPosition()
+                  });
+                  google.maps.event.addListenerOnce(infoWindow, 'closeclick', function() {
+                      markers[this.id].setVisible(true);
+                  });
+                  this.setVisible(false);
+                  infoWindow.open(map);
+              });
+              geocoder = new google.maps.Geocoder();
+              directionsService = new google.maps.DirectionsService();
+              directionsDisplay = new google.maps.DirectionsRenderer({
+                  suppressMarkers: false
+              });
+              directionsDisplay.setMap(map);
+              directionsDisplay.setPanel(document.getElementById('show-results'));
+          }
+      }
+      getInfo();
+      $('.js-query').val('');
+  });
 }
